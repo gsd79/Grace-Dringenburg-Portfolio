a5a6bb94ea04f702186588094692ea195300d22d
Grace <cgracesharpe@gmail.com>
2022-04-26 23:11:45 -0400

    GD: Begun working on nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import Header from './components/Header';
+import Nav from './components/Nav';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
 
 function App() {
+  const [categories] = useState([
+    {name: 'About Me'},
+    {name: 'Portfolio'}, //projects
+    {name: 'Resume'}, //about me
+    {name: 'Contact'}, 
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
+      {/* <Header> */}
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      {/* </Header> */}
+      <main>
+        {/* <Projects currentCategory={currentCategory}></Projects>
+        <About></About>
+        <Contact></Contact>
+        <Footer></Footer>
+         */}
+      </main>
     </div>
   );
 }
